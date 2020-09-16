66ce57e8c47cb9ea29c09a985964aaa824fc4129
Moisito12 <moismm1996@outlook.com>
2020-09-15 19:50:27 -0500

    Error handling validate ready

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import 'express-async-errors';
 import { json } from "body-parser";
 import { currentUserRoute } from "./routes/current-user";
 import { signinRoute } from "./routes/signin";
 import { signupRoute } from "./routes/signup";
 import { signoutRoute } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -12,6 +15,12 @@ app.use(currentUserRoute);
 app.use(signinRoute);
 app.use(signupRoute);
 app.use(signoutRoute);
+
+app.all("*", async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
